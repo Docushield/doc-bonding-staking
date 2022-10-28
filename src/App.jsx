34a552a216b7d0eec73f37afa3d8eaa2e01e6e8f
Sterling Long <sterling.long@lztek.io>
2022-10-28 11:52:55 -0600

    Netlify build

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
             </FlexRow>
             <FlexRow>
             <StakingPool 
-                title="UNLOCKED STAKING"
+                title="UNLOCKED STAKING" 
                 poolName={[
                   import.meta.env.VITE_STAKING_POOL_UNLOCKED_NAME, 
                   import.meta.env.VITE_STAKING_POOL_UNLOCKED_NAME_2
